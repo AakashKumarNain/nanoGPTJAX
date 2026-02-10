793e6f0449a8080c9554b92dd6434ec5c01aa772
AakashKumarNain <aakashnain@outlook.com>
2026-02-10 22:23:46 +0530

    1. Ensure data iterator retsoration is optional. This will come handy during mid-training or post-training where we may want to load the model weights and the optimizer states from a checkpoint to resume training, but the data iterator will be different, so we will not restore that. 2. Add function to load only optimizer state as well.

--- nanogpt/checkpoint_utils.py
+++ nanogpt/checkpoint_utils.py
@@ -21,8 +21,9 @@ def get_sharding_for_checkpoint(x, mesh):
         return NamedSharding(mesh, P())
 
 
-def load_checkpoint(mngr, step, model, optim_state, mesh, ds_iter):
-    """Load state both for the model wieghts and the optimizer state from a given step.
+def load_checkpoint(mngr, step, model, optim_state, mesh, ds_iter=None):
+    """Load the weights, the optimizer state, and (optional) data iterator state
+    from a given checkpoint.
 
     Args:
         mngr: Checkpoint manager instance.
@@ -30,7 +31,8 @@ def load_checkpoint(mngr, step, model, optim_state, mesh, ds_iter):
         model: Pytree  containing params.
         optim_state: Current optimizer state.
         mesh: Current mesh where the model and optimizer state is alive.
-        ds_iter: The data iterator whose state is to be restored from this checkpoint
+        ds_iter (optional): The data iterator whose state is to be restored from
+            this checkpoint. Defaults to `None`.
 
     Returns:
         Tuple of (Restored weights, restored optim_state, restored ds_iter)
@@ -46,22 +48,41 @@ def load_checkpoint(mngr, step, model, optim_state, mesh, ds_iter):
         lambda s: ocp.ArrayRestoreArgs(sharding=get_sharding_for_checkpoint(s, mesh)),
         optim_state,
     )
-    restore_items = ocp.args.Composite(
-        params=ocp.args.PyTreeRestore(
-            item=params_item,
-            transforms=params_transforms,
-            restore_args=params_restore_args,
-        ),
-        optim_state=ocp.args.PyTreeRestore(
-            item=optim_item,
-            transforms=optim_transforms,
-            restore_args=optim_restore_args,
-        ),
-        ds=grain.checkpoint.CheckpointRestore(ds_iter),
-    )
-    restored = mngr.restore(step, args=restore_items)
-    print(f"Restoring checkpoint from step {step} is complete!")
-    return restored.params, restored.optim_state, restored.ds
+    if ds_iter is not None:
+        restore_items = ocp.args.Composite(
+            params=ocp.args.PyTreeRestore(
+                item=params_item,
+                transforms=params_transforms,
+                restore_args=params_restore_args,
+            ),
+            optim_state=ocp.args.PyTreeRestore(
+                item=optim_item,
+                transforms=optim_transforms,
+                restore_args=optim_restore_args,
+            ),
+            ds=grain.checkpoint.CheckpointRestore(ds_iter),
+        )
+        restored = mngr.restore(step, args=restore_items)
+        print(
+            f"Restoring model, optim_state, and data_iter from step {step} is complete!"
+        )
+        return restored.params, restored.optim_state, restored.ds
+    else:
+        restore_items = ocp.args.Composite(
+            params=ocp.args.PyTreeRestore(
+                item=params_item,
+                transforms=params_transforms,
+                restore_args=params_restore_args,
+            ),
+            optim_state=ocp.args.PyTreeRestore(
+                item=optim_item,
+                transforms=optim_transforms,
+                restore_args=optim_restore_args,
+            ),
+        )
+        restored = mngr.restore(step, args=restore_items)
+        print(f"Restoring model and optim_state from step {step} is complete!")
+        return restored.params, restored.optim_state
 
 
 def load_weights_from_checkpoint(path, sharding):
@@ -83,5 +104,34 @@ def load_weights_from_checkpoint(path, sharding):
             path,
             args=ocp.args.PyTreeRestore(
                 item=item, transforms=transforms, restore_args=restore_args
+            ),
+        )
+
+
+def load_optim_state_from_checkpoint(path, optim_state, mesh):
+    """Load optimizer state from a given checkpoint.
+
+    Args:
+        path: Path to the saved pytree checkpointed during training.
+        optim_state: The current optimizer state.
+        mesh: Mesh of the current optimizer state
+
+    Returns:
+        PyTree with optim state loaded from the given checkpoint.
+    """
+
+    print(f"Restoring optimizer state from checkpoint: {path}")
+    ioptim_item, optim_transforms = optim_state, None
+    optim_restore_args = jax.tree.map(
+        lambda s: ocp.ArrayRestoreArgs(sharding=get_sharding_for_checkpoint(s, mesh)),
+        optim_state,
+    )
+    with ocp.PyTreeCheckpointer() as ckptr:
+        return ckptr.restore(
+            path,
+            args=ocp.args.PyTreeRestore(
+                item=optim_state,
+                transforms=optim_transforms,
+                restore_args=optim_restore_args,
             ),
         )
